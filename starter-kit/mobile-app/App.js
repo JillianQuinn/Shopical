1638cd9d408b28d32091e9212c2454dc8d4245aa
Jillian Smith <jilliansmith@ibm.com>
2020-07-15 13:07:15 -0700

    Created Home/Title Page

--- starter-kit/mobile-app/App.js
+++ starter-kit/mobile-app/App.js
@@ -2,17 +2,13 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoadingScreen from './src/screens/loading';
 import Home from './src/screens/home';
-import Chat from './src/screens/chat';
-import Map from './src/screens/map';
 
-import { HomeIcon, ChatIcon, MapIcon } from './src/images/svg-icons';
+import { HomeIcon } from './src/images/svg-icons';
 
-const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const tabBarOptions = {
@@ -37,33 +33,7 @@ const TabLayout = () => (
         tabBarIcon: ({color}) => (<HomeIcon fill={color}/>)
       }}
     />
-    <Tab.Screen
-      name='Chat'
-      component={ChatStackLayout}
-      options={{
-        tabBarIcon: ({color}) => (<ChatIcon fill={color} />)
-      }}
-    />
-    <Tab.Screen
-      name='Map'
-      component={MapStackLayout}
-      options={{
-        tabBarIcon: ({color}) => (<MapIcon fill={color} />)
-      }}
-    />
   </Tab.Navigator>
-);
-
-const ChatStackLayout = () => (
-  <Stack.Navigator>
-    <Stack.Screen name='Ask a Question' component={Chat} />
-  </Stack.Navigator>
-);
-
-const MapStackLayout = () => (
-  <Stack.Navigator>
-    <Stack.Screen name='Locate Assistance' component={Map} />
-  </Stack.Navigator>
 );
 
 const App = () => {
@@ -81,7 +51,7 @@ const App = () => {
   } else {
     return (
       <NavigationContainer>
-        <TabLayout/>
+        <Home/>
       </NavigationContainer>
     );
   }
---
+++ starter-kit/mobile-app/src/Components/CustomButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const CustomButton = props => {
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={{...styles.button, ...props.style}}>
+                <Text style={{...styles.buttonText, ...props.textStyling}}>
+                    {props.title}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "#EEEEEE",
+        paddingVertical: 12,
+        paddingHorizontal: 25, 
+        borderRadius: 25
+    },
+    buttonText: {
+        color: "#5E87D9",
+        fontSize: 18,
+        textAlign:'center'
+    }
+});
+
+export default CustomButton
